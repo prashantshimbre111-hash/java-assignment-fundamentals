5c09e8c7907a68ac201adb9ae6aa0affacc5c0a2
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-23 17:00:12 +0530

    Implement identify either the number is positive,neative or zero with if else condition

---
+++ src/assignments/java_practice/ifelseassg/PositiveNegativeZero.java
@@ -0,0 +1,21 @@
+package assignments.java_practice.ifelseassg;
+
+public class PositiveNegativeZero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 0;
+		
+		if(a>0) {
+			System.out.println("it is positive number");
+		}
+		else if(a<0) {
+			System.out.println("it is negative number");
+
+	}
+		else if(a == 0) {
+			System.out.println("it it zero");
+		}
+
+}}
