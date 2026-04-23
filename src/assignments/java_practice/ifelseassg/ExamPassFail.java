5361cd8385290519e72361cb7625b5d1b031ddec
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-23 17:42:07 +0530

    Implement Exam Pass/Fail with if else condition

---
+++ src/assignments/java_practice/ifelseassg/ExamPassFail.java
@@ -0,0 +1,21 @@
+package assignments.java_practice.ifelseassg;
+
+public class ExamPassFail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int math = 87;
+		int sci = 32;
+		int eng = 98;
+		
+		if(math >=33 && sci >=33 && eng >=33) {
+			System.out.println("pass");
+		}
+		else {
+			System.out.println("fail");
+		}
+
+	}
+
+}
