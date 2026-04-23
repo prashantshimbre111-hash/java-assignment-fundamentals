4efe4cafb9a098cd7689798001a13705f83dbaf6
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-23 14:52:24 +0530

    implement equilateral traingle with if else condition

---
+++ src/assignments/java_practice/ifelseassg/equilateraltraingle.java
@@ -0,0 +1,25 @@
+package assignments.java_practice.ifelseassg;
+
+public class equilateraltraingle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//4.	Triangle Validity
+		//Input 3 angles.
+		//Check if they form a valid triangle (sum = 180 and all > 0).
+
+		int a = 50;
+		int b = 75;
+		int c = 55;
+		
+		if(a+b+c == 180) {
+			System.out.println("sum is 180 " );
+		}
+		else {
+			System.out.println("sum isn't 180");
+		}
+
+	}
+
+}
