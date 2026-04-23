14baf4266b99fe729aef7172fd4ec43e44670ca7
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-23 16:43:21 +0530

    Implement odd difference with if else condition

---
+++ src/assignments/java_practice/ifelseassg/odddifferenceevaluator.java
@@ -0,0 +1,26 @@
+
+	package assignments.java_practice.ifelseassg;
+
+	public class odddifferenceevaluator {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			int a = 50;
+			int b = 49;
+
+			if (a % 2 == 1) {
+				if (b % 2 == 1) {
+					System.out.println("both are odd number : " + (a - b));
+				}
+
+			}
+			else {
+				System.out.println("invalid");
+			}
+
+		}
+	}
+
+
+
