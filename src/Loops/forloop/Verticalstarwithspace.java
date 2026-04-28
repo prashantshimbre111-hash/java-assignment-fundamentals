380f435a804f7a920196686e6495085ba393d5af
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 16:56:56 +0530

    Implement 5 stars vertically with one space in each star

---
+++ src/Loops/forloop/Verticalstarwithspace.java
@@ -0,0 +1,12 @@
+package Loops.forloop;
+
+public class Verticalstarwithspace {
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5; i++) {
+
+			System.out.println("* ");
+		}
+	}
+
+}
