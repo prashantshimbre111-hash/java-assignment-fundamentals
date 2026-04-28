79edca324fa89842ef45799207e2c47218715dff
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 15:27:55 +0530

    Implement a program to print all even numbers between 1 to 100 without using if condition.

--- src/Loops/forloop/Evenwithif.java
+++ src/Loops/forloop/Evenwithif.java
@@ -1,14 +1,19 @@
 package Loops.forloop;
 
-public class Even {
+public class Evenwithif {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		for(int i = 2 ; i <= 100 ; i+=2) {
-			System.out.println(i);
-		}
 
+		
+		for(int i = 2 ; i<=100 ; i+=2) {
+			
+			if (i%2==0) {
+				
+			}
+			System.out.println(i);
+			
+		}
 	}
 
 }
