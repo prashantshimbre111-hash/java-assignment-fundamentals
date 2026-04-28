bfaf4c589777181f113551e38298c927c17990dc
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 16:34:39 +0530

    Implement a Java program to display the cube of the given number up to an integer with user input

---
+++ src/Loops/forloop/Cubewithinput.java
@@ -0,0 +1,22 @@
+package Loops.forloop;
+
+import java.util.Scanner;
+
+public class Cubewithinput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number here");
+		int n = sc.nextInt();
+		
+		for (int i = 0 ; i<=n ; i++) {
+			
+			int cube = i*i*i;
+			
+			System.out.println(cube);
+		}
+
+	}
+
+}
