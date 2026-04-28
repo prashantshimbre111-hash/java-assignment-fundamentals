78d782067f5b94491c66f0acc79e5035747719b3
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 15:33:05 +0530

    Implement  a program to print all odd numbers between 1 to 100 without using if condition

---
+++ src/Loops/forloop/Odd.java
@@ -0,0 +1,13 @@
+package Loops.forloop;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1 ; i<=100 ; i+=2) {
+			System.out.println(i);
+		}
+
+	}
+
+}
