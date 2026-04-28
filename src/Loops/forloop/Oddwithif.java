67926ee2065f83f97664d277e5ecdba4107fd6dc
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 15:31:23 +0530

    Implement a program to print all odd numbers between 1 to 100 using if condition.

---
+++ src/Loops/forloop/Oddwithif.java
@@ -0,0 +1,13 @@
+package Loops.forloop;
+
+public class Oddwithif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1 ; i <= 100 ; i+=2) {
+			if(i%2==1) {}
+		System.out.println(i);
+	}}
+
+}
