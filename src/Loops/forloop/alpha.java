1f5c9ef5205506b016db92e684463ad3d064622a
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 15:16:50 +0530

    Implement  a program to print all alphabets from a to z.

---
+++ src/Loops/forloop/alpha.java
@@ -0,0 +1,14 @@
+package Loops.forloop;
+
+public class alpha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(char i = 'a' ; i <= 'z' ; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
