79a928193fecfa80ce0d0f33d5e3867c9509261f
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 16:48:06 +0530

    implement  Print 10 stars horizontally

---
+++ src/Loops/forloop/Horizantalstar.java
@@ -0,0 +1,12 @@
+package Loops.forloop;
+
+public class Horizantalstar {
+	public static void main(String[] args) {
+		
+		for (int i = 1 ; i <2 ; i++ ) {
+			
+			System.out.println("**********");
+		}
+	}
+
+}
