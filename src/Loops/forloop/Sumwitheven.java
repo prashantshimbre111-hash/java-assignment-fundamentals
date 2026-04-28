1ee031b1628aa5d45cb6b93bb260ed785c55bf82
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 15:47:19 +0530

    Implement a program to find sum of all even numbers between 1 to n

--- src/Loops/forloop/Sumwitheven.java
+++ src/Loops/forloop/Sumwitheven.java
@@ -2,22 +2,22 @@ package Loops.forloop;
 
 import java.util.Scanner;
 
-public class Sumwithforloop {
+public class Sumwitheven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter number here : ");
+		System.out.println("please enter number here : ");
 		int n = sc.nextInt();
 		int sum = 0;
-		for (int i = 1; i <= n; i++) {
 
+		for (int i = 2; i <= n; i += 2) {
 			sum += i;
 
-			
 		}
 		System.out.println(sum);
-		sc.close();
+
 	}
 
 }
