4cde043091189c489f6918aa873b4668fe9d903a
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-28 16:30:07 +0530

    Implement a Java program to display the cube of the given number up to an integer

---
+++ src/Loops/forloop/Cubes.java
@@ -0,0 +1,14 @@
+package Loops.forloop;
+
+public class Cubes {
+	public static void main(String[] args) {
+		
+		for (int i = 1 ; i <=100 ; i++) {
+			int cube = i*i*i;
+			System.out.println(cube);
+		}
+		
+		
+	}
+
+}
