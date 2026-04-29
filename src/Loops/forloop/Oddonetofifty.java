579c10a67f03d20c647d7bf4723be95818d1ce2a
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 16:38:55 +0530

    Print odd numbers from 1 to 50.

---
+++ src/Loops/forloop/Oddonetofifty.java
@@ -0,0 +1,14 @@
+package Loops.forloop;
+
+public class Oddonetofifty {
+	
+	// 	Print odd numbers from 1 to 50.
+	 public static void main(String[] args) {
+		
+		 for(int i = 1 ; i<=50 ; i++) {
+			 if(i%2==1) {
+				 System.out.println(i);
+			 }
+		 }
+	}
+}
