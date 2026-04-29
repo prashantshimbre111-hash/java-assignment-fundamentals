46a65b7bc4153040f15f0d7e4f2ecba683eff679
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 16:33:25 +0530

    numbers from 10 to 1 in reverse using a for loop.

---
+++ src/Loops/forloop/Reversenumber.java
@@ -0,0 +1,14 @@
+package Loops.forloop;
+
+public class Reversenumber {
+	
+	// 	Print numbers from 10 to 1 in reverse using a for loop.
+	
+	public static void main(String[] args) {
+		
+		for(int i = 10 ; i>=1 ; i--) {
+			
+			System.out.println(i);
+		}
+	}
+}
