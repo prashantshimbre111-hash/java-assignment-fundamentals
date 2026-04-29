59cfa1bd692b3e78f20ece793f20d1f3758c0b73
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 16:44:29 +0530

    Print squares of numbers from 1 to 	n (user input).

---
+++ src/Loops/forloop/Square.java
@@ -0,0 +1,20 @@
+package Loops.forloop;
+
+import java.util.Scanner;
+
+public class Square {
+	// 	Print squares of numbers from 1 to 	n (user input).
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("please enter number : ");
+		int n = sc.nextInt();
+		
+		for(int i = 1 ; i <=1; i++) {
+			System.out.println("square of number : " + n*n);
+		}
+
+	}
+
+}
