f0da213215315f60b6c7a9125d8285fdd5359533
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 16:30:49 +0530

    impemented numbers from 1 to 10 using a for loop.

---
+++ src/Loops/forloop/onttoten.java
@@ -0,0 +1,15 @@
+package Loops.forloop;
+
+public class onttoten {
+	
+	//1. Print numbers from 1 to 10 using a for loop.
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i<=10 ; i++) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
