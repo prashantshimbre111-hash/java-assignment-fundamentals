c0ef5e9d54beccf770ab4cca97c83064ad9bb526
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-29 16:36:50 +0530

    Print even numbers from 1 to 50.

---
+++ src/Loops/forloop/evenonetofifty.java
@@ -0,0 +1,14 @@
+package Loops.forloop;
+
+public class evenonetofifty {
+	// Print even numbers from 1 to 50.
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1 ; i<=50 ; i++) {
+			
+			if(i%2==0) {System.out.println(i);}
+			
+		}
+	}
+}
