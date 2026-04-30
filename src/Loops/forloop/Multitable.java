9f8e11c6988e7c8608f859c4591cebecba6529cd
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-30 13:31:11 +0530

    print table without user inout with for loop implemented

---
+++ src/Loops/forloop/Multitable.java
@@ -0,0 +1,16 @@
+package Loops.forloop;
+
+public class Multitable {
+	public static void main(String[] args) {
+		
+		//we have print table without user inout with for loop
+		
+		
+		for(int i = 1 ; i<=10 ; i++ ) {
+			
+			System.out.println(i+(i*3));
+			
+		}
+	}
+
+}
