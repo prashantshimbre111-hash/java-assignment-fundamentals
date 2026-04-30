0d87af7d5b4b30d3a0d60f5bebec6c001f9b615d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-04-30 16:15:52 +0530

    count digit in a number with for loop.

--- src/Loops/forloop/countdigit.java
+++ src/Loops/forloop/countdigit.java
@@ -4,27 +4,23 @@ import java.util.Scanner;
 
 public class countdigit {
 	public static void main(String[] args) {
-		
-		//we have to count digit in a number with for loop.
-		
+
+		// we have to count digit in a number with for loop.
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("enter input here : ");
 		int n = sc.nextInt();
-		
-		int count = 0;
-		
-	
-		
-		for (int i = 0 ; n!=0 ; count++) {
-			
-			n =n/10;			
-		
+
+		int count = 0; // initiate count first
+
+		for (int i = 0; n != 0; count++) { //then input is not zero and count ++ for run loop of count which we have to print
+
+			n = n / 10;              //means if input is divide as per their digit
+
 		}
 		System.out.println(count);
-		
-	
+
 	}
 
 }
-
