30a05ab85aa49cc09868055326666dd271b8133d
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-05-13 11:49:42 +0530

    .

--- src/Array/Tsetarray.java
+++ src/Array/Tsetarray.java
@@ -4,20 +4,22 @@ public class Tsetarray {
 	
 	public static void main(String[] prashant) {
 		
-		int[] s = {10 , 15 ,  20 , 25 , 30 , 35, 40 , 45 , 50 , 55 , 60 };
+		int[] s = {15 ,  20 , 25 , 30 , 35, 40 , 45 , 50 , 55 , 60 };
 		
 		System.out.println(s[0]);
 		System.out.println(s[4]);
 		System.out.println();
 		
-//		for (int i = 0 ; i <= s.length()-1 ; i++) {
+//		for (int i = 0 ; i <= s.length-1 ; i++) {
 //			
 //			System.out.println(s[i]);
 //			
 //		}
 		for(int i = 0 ; i<=s.length-1 ; i++) {
 			
-			if(i%2==1) {
+			
+			
+			if(s[i]%2==1) {
 				
 				System.out.println(s[i]);
 			}
